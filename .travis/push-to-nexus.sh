f1d172b77d1cb46de76ded65bd367a60aa38d3c0
Jakub Scholz <www@scholzj.com>
2019-09-16 22:11:30 +0200

    Fix push to nexus
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -3,7 +3,7 @@
 openssl aes-256-cbc -K $encrypted_9bb2e0bcb75b_key -iv $encrypted_9bb2e0bcb75b_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn -B -DskipTests -s ./.travis/settings.xml  -pl '!kafka-oauth-examples-consumer,!kafka-oauth-examples-producer' -P ossrh clean package gpg:sign deploy
+GPG_EXECUTABLE=gpg mvn -B -DskipTests -s ./.travis/settings.xml  -pl '!examples/producer','!examples/consumer' -P ossrh clean package gpg:sign deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
