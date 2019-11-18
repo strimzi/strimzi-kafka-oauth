86314243205bedf16275a9701014f3e921454af8
Marko Strukelj <marko.strukelj@gmail.com>
2019-11-18 21:24:59 +0100

    Fix kafka startup script in examples (#18)
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- examples/docker/kafka-oauth-strimzi/kafka/start.sh
+++ examples/docker/kafka-oauth-strimzi/kafka/start.sh
@@ -14,4 +14,7 @@ wait_for_url "$URI/realms/${REALM:-demo}" "Waiting for realm '${REALM}' to be av
 
 ./simple_kafka_config.sh | tee /tmp/strimzi.properties
 
+# add Strimzi kafka-oauth-* jars and their dependencies to classpath
+export CLASSPATH="/opt/kafka/libs/strimzi/*:$CLASSPATH"
+
 exec /opt/kafka/bin/kafka-server-start.sh /tmp/strimzi.properties
