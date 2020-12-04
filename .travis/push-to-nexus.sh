be60d8b42cce630381ac3bb9367c827b03e124ca
Jakub Scholz <www@scholzj.com>
2020-12-04 20:28:27 +0100

    Update expired signing key (#79)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_9bb2e0bcb75b_key -iv $encrypted_9bb2e0bcb75b_iv -in .travis/signing.gpg.enc -out signing.gpg -d
+openssl aes-256-cbc -K $encrypted_a40d2cfb3073_key -iv $encrypted_a40d2cfb3073_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
 GPG_EXECUTABLE=gpg mvn -B -DskipTests -s ./.travis/settings.xml  -pl '!examples/producer','!examples/consumer' -P ossrh clean package gpg:sign deploy
