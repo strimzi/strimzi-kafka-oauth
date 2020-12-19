8b0b1b7e4842f8744d6febc0ffce33d6771660f1
Marko Strukelj <marko.strukelj@gmail.com>
2020-12-19 23:39:47 +0100

    Add to testsuite its own certificates + add hydra-jwt certificate
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- testsuite/docker/hydra-import/scripts/start.sh
+++ testsuite/docker/hydra-import/scripts/start.sh
@@ -23,7 +23,7 @@ cat < /hydra/certs/ca.crt >> /etc/ssl/certs/ca-certificates.crt
 
 URI=${HYDRA_URI}
 if [ "" == "${URI}" ]; then
-    URI="http://${HYDRA_HOST:-hydra}:4445/clients"
+    URI="http://${HYDRA_HOST:-hydra}:{SERVE_ADMIN_PORT:-4445}/clients"
 fi
 
 wait_for_url $URI "Waiting for Hydra admin REST to start"
