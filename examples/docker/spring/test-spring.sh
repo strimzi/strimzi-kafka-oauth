44f491484c670461fa94d0e4a23d9dc9dec09271
Marko Strukelj <marko.strukelj@gmail.com>
2023-06-23 17:09:44 +0200

    Fix Spring example (debug Travis issue)
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- examples/docker/spring/test-spring.sh
+++ examples/docker/spring/test-spring.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -x
 
 docker rm -f spring
 docker run -d --name spring strimzi/example-spring
-for i in 1 2 3 4 5
+for i in {1..10}
 do
   sleep 1
   RESULT=$(docker logs spring | grep "Started SimpleAuthorizationServerApplication")
-  if [[ "$RESULT" != "" ]]; then
+  if [ "$RESULT" != "" ]; then
      docker rm -f spring
      exit 0
   fi
