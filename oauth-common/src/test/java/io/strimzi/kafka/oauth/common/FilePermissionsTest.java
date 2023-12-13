b5e468ddf14c1f1dfaaa1cc51c406ca4486f68ef
Marko Strukelj <marko.strukelj@gmail.com>
2023-12-13 11:53:27 +0100

    Add file permissions test when symlinks are used
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- oauth-common/src/test/java/io/strimzi/kafka/oauth/common/FilePermissionsTest.java
+++ oauth-common/src/test/java/io/strimzi/kafka/oauth/common/FilePermissionsTest.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileAttribute;
@@ -20,17 +21,19 @@ import java.nio.file.attribute.AclEntry;
 import java.nio.file.attribute.AclEntryType;
 import java.nio.file.attribute.AclFileAttributeView;
 import java.nio.file.attribute.UserPrincipal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Set;
 
 import static io.strimzi.kafka.oauth.common.IOUtil.isFileAccessLimitedToOwner;
+import static java.nio.file.Files.createSymbolicLink;
 
 public class FilePermissionsTest {
 
     @Test
     public void filePermissionCheckTest() throws Exception {
-        // if file does not exist there is nothing to check
+
         String filePath = "target/test1.txt";
         Path file = Paths.get(filePath);
         Assert.assertFalse("File should not yet exist", Files.exists(file));
@@ -53,6 +56,48 @@ public class FilePermissionsTest {
             try {
                 Files.delete(file);
             } catch (Exception ignored) {
+            }
+        }
+    }
+
+    @Test
+    public void filePermissionWithSymlinksCheckTest() throws Exception {
+
+        FileSystem fs = FileSystems.getDefault();
+        Set<String> supportedViews = fs.supportedFileAttributeViews();
+        if (supportedViews.contains("posix")) {
+
+            String filePath = "target/test1.txt";
+            Path file = Paths.get(filePath);
+            Assert.assertFalse("File should not yet exist", Files.exists(file));
+
+            // also check that symlinks don't exist
+            Path link1File = Paths.get("target/link1");
+            Path link2File = Paths.get("target/link2");
+            Assert.assertFalse("Link1 should not yet exist", Files.exists(link1File, LinkOption.NOFOLLOW_LINKS));
+            Assert.assertFalse("Link2 should not yet exist", Files.exists(link2File, LinkOption.NOFOLLOW_LINKS));
+
+            try {
+                createSymbolicLink(link1File, file.getFileName());
+                createSymbolicLink(link2File, link1File.getFileName());
+
+                // This part of the test only works in posix compatible environment
+                // Create a file
+                createFile(file, false);
+                Assert.assertFalse("File should be accessible to group and others", isFileAccessLimitedToOwner(link2File));
+
+                Files.delete(file);
+
+                createFile(file, true);
+                Assert.assertTrue("File should NOT be accessible to group and others", isFileAccessLimitedToOwner(file));
+
+            } finally {
+                for (Path f: Arrays.asList(file, link1File, link2File)) {
+                    try {
+                        Files.delete(f);
+                    } catch (Exception ignored) {
+                    }
+                }
             }
         }
     }
