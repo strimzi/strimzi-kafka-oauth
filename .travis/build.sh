46932db04d0ab56c24c5f481bcfd45c8c3359376
Marko Strukelj <marko.strukelj@gmail.com>
2024-02-04 20:52:02 +0100

    Revert "Retry enabling Keycloak rebuild + testsuite run on s390x"
    This reverts commit b5a7f8d062b6b7f1fc6e3675d2edfbad9b0026a8.
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -48,33 +48,7 @@ if [ "$arch" != 'ppc64le' ]; then
 fi
 
 # Run testsuite
-if [ "$arch" == 's390x' ]; then
-  # Build s390x compatible hydra image
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/s390x-linux-gnu/jni
-
-  cd target
-  git clone -b 23.0.5 https://github.com/keycloak/keycloak.git
-  cd keycloak
-  mvn -q -pl quarkus/deployment,quarkus/dist -am -DskipTests clean install
-  cd quarkus/container
-  cp ../dist/target/keycloak-*.tar.gz .
-  docker build --build-arg KEYCLOAK_DIST=$(ls keycloak-*.tar.gz) . -t quay.io/keycloak/keycloak:23.0.5
-  cd ../../../.. && rm -rf target/keycloak
-
-  docker build --target hydra-import -t strimzi-oauth-testsuite/hydra-import:latest -f ./testsuite/docker/hydra-import/Dockerfile.s390x .
-
-  docker build --target oryd-hydra -t oryd/hydra:v1.8.5 -f ./testsuite/docker/hydra-import/Dockerfile.s390x .
-
-  mvn -q test-compile spotbugs:check -e -V -B -f testsuite
-  set +e
-  clearDockerEnv
-  mvn -e -V -B clean install -f testsuite -Pkafka-3_6_1
-  EXIT=$?
-  exitIfError
-  set -e
-
-elif [[ "$arch" != 'ppc64le' ]]; then
-
+if [ "$arch" != 'ppc64le' ] && [ "$arch" != 's390x' ]; then
   mvn test-compile spotbugs:check -e -V -B -f testsuite $MAVEN_EXTRA_ARGS
 
   set +e
