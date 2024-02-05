fda9ece2ca141eb7d36c03c243e31f5359c84cbe
Marko Strukelj <marko.strukelj@gmail.com>
2024-02-05 16:49:52 +0100

    Troubleshoot s390x testsuite build
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

---
+++ .travis/build-s390x.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+export PULL_REQUEST=${PULL_REQUEST:-true}
+export BRANCH=${BRANCH:-main}
+export TAG=${TAG:-latest}
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/s390x-linux-gnu/jni
+
+mkdir target
+cd target
+git clone -b 23.0.5 https://github.com/keycloak/keycloak.git
+cd keycloak
+mvn -e -pl quarkus/deployment,quarkus/dist -am -DskipTests clean install
+cd quarkus/container
+cp ../dist/target/keycloak-*.tar.gz .
+docker build --build-arg KEYCLOAK_DIST=$(ls keycloak-*.tar.gz) . -t quay.io/keycloak/keycloak:23.0.5
+cd ../../../.. && rm -rf target/keycloak
