d665e0adf73d9f3861facdbeeff6a30a7cd55510
Marko Strukelj <marko.strukelj@gmail.com>
2024-02-05 19:02:59 +0100

    Try fix nodejs native rollup package unavailability
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- .travis/build-s390x.sh
+++ .travis/build-s390x.sh
@@ -11,6 +11,22 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/s390x-linux-gnu/jni
 cd target
 git clone -b 23.0.5 https://github.com/keycloak/keycloak.git
 cd keycloak
+
+cd js
+rm pnpm-lock.yaml
+head -n -2 package.json > tmp.txt
+echo '  },
+  "resolutions": {
+    "rollup": "npm:@rollup/wasm-node"
+  },
+  "overrides": {
+    "rollup": "npm:@rollup/wasm-node"
+  }
+}' >> tmp.txt
+mv tmp.txt package.json
+pnpm install --no-frozen-lockfile
+cd ..
+
 mvn -e -pl quarkus/deployment,quarkus/dist -am -DskipTests clean install
 cd quarkus/container
 cp ../dist/target/keycloak-*.tar.gz .
