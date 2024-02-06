57297fd30a769e6d727f477c5887a0aac7daea62
Jakub Scholz <www@scholzj.com>
2024-02-06 15:45:55 +0100

    Prepare Azure CI pipeline for OAuth
    Signed-off-by: Jakub Scholz <www@scholzj.com>

---
+++ .azure/scripts/push-to-nexus.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Build reason: ${BUILD_REASON}"
+echo "Source branch: ${BRANCH}"
+
+GPG_TTY=$(tty)
+export GPG_TTY
+
+echo "$GPG_SIGNING_KEY" | base64 -d > signing.gpg
+gpg --batch --import signing.gpg
+
+GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl '!examples/producer','!examples/consumer' -P ossrh verify deploy
+
+rm -rf signing.gpg
+gpg --delete-keys
+gpg --delete-secret-keys
