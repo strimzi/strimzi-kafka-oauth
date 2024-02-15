744626b23984ecaaa7f0479a807c0bcf34b251a3
Marko Strukelj <marko.strukelj@gmail.com>
2024-02-15 20:49:55 +0100

    Increase wait timeouts for testsuite containers
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- testsuite/keycloak-authz-kraft-tests/src/test/java/io/strimzi/testsuite/oauth/authz/kraft/KeycloakKRaftAuthorizationTests.java
+++ testsuite/keycloak-authz-kraft-tests/src/test/java/io/strimzi/testsuite/oauth/authz/kraft/KeycloakKRaftAuthorizationTests.java
@@ -46,7 +46,7 @@ public class KeycloakKRaftAuthorizationTests {
 
                     // ensure kafka has started
                     .waitingFor("kafka", Wait.forLogMessage(".*started \\(kafka.server.KafkaRaftServer\\).*", 1)
-                            .withStartupTimeout(Duration.ofSeconds(120)));
+                            .withStartupTimeout(Duration.ofSeconds(300)));
 
                     // ensure ACLs for user 'alice' have been added
                     //   Moved into test code: waitForACLs()
