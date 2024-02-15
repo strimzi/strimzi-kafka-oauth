cfb94a62ae7df415f3abbc3927eb5ee1f7de287f
Marko Strukelj <marko.strukelj@gmail.com>
2024-02-15 12:13:08 +0100

    Enable s390x testsuite run
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -48,7 +48,27 @@ if [ "$arch" != 'ppc64le' ]; then
 fi
 
 # Run testsuite
-if [ "$arch" != 'ppc64le' ] && [ "$arch" != 's390x' ]; then
+if [ "$arch" == 's390x' ]; then
+    # Build s390x compatible hydra image
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/s390x-linux-gnu/jni
+
+    cd target
+    git clone -b 23.0.5 https://github.com/keycloak/keycloak.git
+    cd keycloak/quarkus/container
+    docker build . -t quay.io/keycloak/keycloak:23.0.5
+    cd ../../../.. && rm -rf target/keycloak
+
+    docker build --target hydra-import -t strimzi-oauth-testsuite/hydra-import:latest -f ./testsuite/docker/hydra-import/Dockerfile.s390x .
+    docker build --target oryd-hydra -t oryd/hydra:v1.8.5 -f ./testsuite/docker/hydra-import/Dockerfile.s390x .
+
+    mvn test-compile spotbugs:check -e -V -B -f testsuite
+    set +e
+    clearDockerEnv
+    mvn -e -V -B clean install -f testsuite -Pkafka-3_6_1
+    EXIT=$?
+    exitIfError
+    set -e
+elif [[ "$arch" != 'ppc64le' ]]; then
   mvn test-compile spotbugs:check -e -V -B -f testsuite $MAVEN_EXTRA_ARGS
 
   set +e
