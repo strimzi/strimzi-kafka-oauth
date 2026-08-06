71fbca618251597eea78b3d4f6f69b18defa9069
Marko Strukelj <marko.strukelj@gmail.com>
2026-08-06 16:23:21 +0200

    Upgrade Spring example (#324)
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- examples/docker/spring/src/main/java/io/strimzi/examples/spring/SpringAuthorizationServerApplication.java
+++ examples/docker/spring/src/main/java/io/strimzi/examples/spring/SpringAuthorizationServerApplication.java
@@ -3,7 +3,7 @@ package io.strimzi.examples.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 
 @SpringBootApplication
 public class SpringAuthorizationServerApplication {
